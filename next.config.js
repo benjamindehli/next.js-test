9c4664b980640e9d94c56ea0669f76d2bf877d3b
Benjamin Dehli <superelg@gmail.com>
2022-04-03 22:13:42 +0200

    Add multilingual support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'no'],
+    defaultLocale: 'no',
+    localeDetection: false
+  },
 }
 
 module.exports = nextConfig
