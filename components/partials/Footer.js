becdfe344cadfde64ea042b108066e78dbe60f2e
Benjamin Dehli <superelg@gmail.com>
2022-04-03 22:37:17 +0200

    Add footer component

---
+++ components/partials/Footer.js
@@ -0,0 +1,14 @@
+// Stylesheets
+import style from './Footer.module.scss';
+
+const Footer = () => {
+
+    return (
+        <div id="contact" className={style.footer}>
+            footer
+        </div>
+    )
+
+}
+
+export default Footer
---
+++ helpers/timeFormatter.js
@@ -0,0 +1,7 @@
+export const timestampToDateInputValue = (timestamp) => {
+    const date = new Date(timestamp);
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  }
