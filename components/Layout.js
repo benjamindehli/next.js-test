3d8247c2f8127026e6bc930dfb69860c50b3ee47
Benjamin Dehli <superelg@gmail.com>
2022-04-03 22:19:46 +0200

    Add layout component

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+// Components
+import NavigationBar from "components/partials/NavigationBar";
+
+// Stylesheets
+import style from "components/Layout.module.scss";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={style.layout}>
+      <NavigationBar />
+
+      <div className={style.layoutBody}>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
