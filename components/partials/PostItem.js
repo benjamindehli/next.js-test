37ea5538b06753eafc361f5c49fbb7f0030dfdd2
Benjamin Dehli <superelg@gmail.com>
2022-04-05 00:06:36 +0200

    Use lists and list items for posts

--- components/partials/PostItem.js
+++ components/partials/PostItem.js
@@ -1,25 +1,76 @@
 // Dependencies
 import { Fragment } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
+import Image from "next/image";
+
+// Components
+import ListItemThumbnail from "components/template/List/ListItem/ListItemThumbnail";
+import ListItemContent from "components/template/List/ListItem/ListItemContent";
+import ListItemContentHeader from "components/template/List/ListItem/ListItemContent/ListItemContentHeader";
+import ListItemContentBody from "components/template/List/ListItem/ListItemContent/ListItemContentBody";
+import ListItemActionButtons from "components/template/List/ListItem/ListItemActionButtons";
+import Button from "components/template/Button";
 
 // Helpers
-import { convertToUrlFriendlyString } from "helpers/urlFormatter";
-
+import { getLocaleSlug } from "helpers/urlFormatter";
+import { getPrettyDate } from "helpers/dateFormatter";
 
 const PostItem = (props) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const locale = router.locale;
+    const { locale, defaultLocale } = router;
 
-  const postSlug = convertToUrlFriendlyString(props.post.title[locale]);
+    const postDate = new Date(props.post.timestamp);
 
-  return (
-    <Fragment>
-      <h2> {props.post.title[locale]}</h2>
-      <Link href={`/posts/${postSlug}`}>{props.post.title[locale]}</Link>
-    </Fragment>
-  );
+    const link = {
+        to: `/${locale}/posts/${props.post.slug[locale]}/`,
+        title: props.post.title[locale]
+    };
+
+    const languageSlug = getLocaleSlug(locale, defaultLocale);
+
+    const renderContentLink = () => {
+        const contentLink = props.post.link;
+        return contentLink.internal ? (
+            <Button href={`/${languageSlug}${contentLink.url[locale]}`} title={contentLink.text[locale]} buttontype="minimal">
+                {contentLink.text[locale]}
+            </Button>
+        ) : (
+            <Button href={contentLink.url} target="_blank" rel="noopener noreferrer" title={contentLink.text[locale]}>
+                <a>{contentLink.text[locale]}</a>
+            </Button>
+        );
+    };
+
+    const renderPostThumbnail = () => {
+        const filename = props.post.thumbnailFilename + "_540.jpg";
+        const imageProps = {
+          priority: !!props.fulscreen,
+          sizes: !!props.fullscreen ? "540px" : "(max-width: 599px) 55px, 350px"
+        }
+        return <Image {...imageProps} width="540" height="400" quality="60" src={`/images/posts/${filename}`} alt={props.post.thumbnailDescription} />;
+    };
+
+    return (
+        <Fragment>
+            <ListItemThumbnail fullscreen={props.fullscreen} link={link}>
+                {renderPostThumbnail()}
+            </ListItemThumbnail>
+            <ListItemContent fullscreen={props.fullscreen}>
+                <ListItemContentHeader fullscreen={props.fullscreen} link={props.link}>
+                    <h2>{props.post.title[locale]}</h2>
+                    <time dateTime={postDate.toISOString()}>{getPrettyDate(postDate, locale)}</time>
+                </ListItemContentHeader>
+                <ListItemContentBody fullscreen={props.fullscreen}>
+                    {props.post.content[locale].split("\n").map((paragraph, key) => {
+                        return <p key={key}>{paragraph}</p>;
+                    })}
+                </ListItemContentBody>
+                {props.post.link && props.fullscreen ? <ListItemActionButtons fullscreen={props.fullscreen}>{renderContentLink()}</ListItemActionButtons> : ""}
+            </ListItemContent>
+            <h2> {props.post.title[locale]}</h2>
+        </Fragment>
+    );
 };
 
 export default PostItem;
--- components/partials/PostList.js
+++ components/partials/PostList.js
@@ -1,20 +1,27 @@
-// Dependencies
-import { Fragment } from "react";
-import { useRouter } from "next/router";
-
 // Components
+import Container from "components/template/Container";
+import List from "components/template/List";
+import ListItem from "components/template/List/ListItem";
 import PostItem from "./PostItem";
 
 const PostList = (props) => {
-  const router = useRouter();
+    const renderPosts = () => {
+        return props.posts?.length
+            ? props.posts.map((post) => {
+                  return (
+                      <ListItem key={post.id}>
+                          <PostItem post={post} />
+                      </ListItem>
+                  );
+              })
+            : "";
+    };
 
-  return (
-    <Fragment>
-      {props.posts.map((post) => {
-        return <PostItem key={post.id} post={post} />;
-      })}
-    </Fragment>
-  );
+    return (
+        <Container blur={props.blur}>
+            <List>{renderPosts()}</List>
+        </Container>
+    );
 };
 
 export default PostList;
