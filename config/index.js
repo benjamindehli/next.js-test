c282bc7c7bf16bb4d7438c5a61cd93360c3cb089
Benjamin Dehli <superelg@gmail.com>
2022-04-04 23:55:41 +0200

    Add config variables

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const environment = process.env.NODE_ENV;
+const productionUrl = process.env.PRODUCTION_URL;
+const dbClusterUrl = process.env.DB_CLUSTER_URL;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const isDev = environment !== "productions";
+
+export const server = isDev ? "http://localhost:3000" : productionUrl;
+
+export const dbConnectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbClusterUrl}/${dbName}?retryWrites=true&w=majority`;
