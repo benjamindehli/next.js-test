6f4d130cd815d72584b0f9d92e0fd90c6f4dcd4d
Benjamin Dehli <superelg@gmail.com>
2022-04-05 23:51:35 +0200

    Add modal for selected post item

--- pages/posts/[slug]/index.js
+++ pages/posts/[slug]/index.js
@@ -1,77 +1,92 @@
 // Dependencies
 import { Fragment } from "react";
-import { useRouter } from "next/router";
 import { MongoClient } from "mongodb";
+import { useRouter } from "next/router";
 
 // Config
-import { server, dbConnectionString } from "config";
+import { dbConnectionString } from "config";
 
 // Componets
 import PostItem from "components/partials/PostItem";
 import PostList from "components/partials/PostList";
+import Modal from "components/template/Modal";
 
 // Helpers
-import { convertToUrlFriendlyString } from "helpers/urlFormatter";
 import { getOneInCollection, getAllInCollection } from "helpers/databaseHelpers";
+import { getLocaleSlug, getSlugForNeighbourItems } from "helpers/urlHelpers";
 
 const Post = (props) => {
-  const router = useRouter();
+    const router = useRouter();
+    const { locale } = router;
 
-  return (
-    <Fragment>
-      <PostItem post={props.post} />
-      <PostList posts={props.posts} />
-    </Fragment>
-  );
+    const handleClickOutside = () => {
+        router.push(`/${props.localeSlug}posts/`);
+    };
+    const arrowLeftLink = props.previousPostSlug?.length
+        ? `/${props.localeSlug}posts/${props.previousPostSlug}/`
+        : null;
+    const arrowRightLink = props.nextPostSlug?.length ? `/${props.localeSlug}posts/${props.nextPostSlug}/` : null;
+
+    return (
+        <Fragment>
+            <Modal
+                onClickOutside={handleClickOutside}
+                maxWidth="540px"
+                arrowLeftLink={arrowLeftLink}
+                arrowRightLink={arrowRightLink}
+                selectedLanguageKey={locale}
+            >
+                <PostItem post={props.post} fullscreen />
+            </Modal>
+            <PostList posts={props.posts} blur />
+        </Fragment>
+    );
 };
 
 export default Post;
 
 // This function gets called at build time
 export async function getStaticPaths({ locales }) {
-  // Call an external API endpoint to get posts
-  const res = await fetch(`${server}/api/posts`);
-  const posts = await res.json();
+    const client = await MongoClient.connect(dbConnectionString);
+    const posts = await getAllInCollection(client, "posts");
+    client.close();
 
-  // Get the paths we want to pre-render based on posts
-  const paths = [];
+    // Get the paths we want to pre-render based on posts
+    const paths = [];
 
-  posts.forEach((post) => {
-    locales.forEach((locale) => {
-      const slug = convertToUrlFriendlyString(post.title[locale]);
-      paths.push({
-        params: { slug },
-        locale,
-      });
+    posts.forEach((post) => {
+        locales.forEach((locale) => {
+            const slug = post.slug?.[locale];
+            if (slug) {
+                paths.push({
+                    params: { slug },
+                    locale
+                });
+            }
+        });
     });
-  });
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false };
 }
 
 export const getStaticProps = async (context) => {
-  const client = await MongoClient.connect(dbConnectionString);
-  const post = await getOneInCollection(
-    client,
-    "posts",
-    context.params.slug,
-    context.locale
-  );
-  const posts = await getAllInCollection(client, "posts");
-  client.close();
+    const client = await MongoClient.connect(dbConnectionString);
+    const post = await getOneInCollection(client, "posts", context.params.slug, context.locale);
+    const posts = await getAllInCollection(client, "posts");
+    client.close();
 
+    const slugForNeighbourItems = getSlugForNeighbourItems(post, posts, context.locale);
+    const localeSlug = getLocaleSlug(context.locale, context.defaultLocale);
 
-
-  /*const req = await fetch(
-    `${server}/api/posts?slug=${context.params.slug}&slugLocale=${context.locale}`
-  );
-  const post = await req.json();*/
-  return {
-    props: {
-      posts,
-      post,
-    },
-  };
+    return {
+        props: {
+            posts,
+            post,
+            previousPostSlug: slugForNeighbourItems.previousItemSlug,
+            nextPostSlug: slugForNeighbourItems.nextItemSlug,
+            localeSlug
+        }
+    };
 };
