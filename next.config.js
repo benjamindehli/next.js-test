e643e48f7fcf4230c17faac89e8c17f83fa3ee0c
Benjamin Dehli <superelg@gmail.com>
2022-04-10 18:44:26 +0200

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'no'],
-    defaultLocale: 'no',
-    localeDetection: false
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [350, 540, 945],
-    imageSizes: [16, 32, 48, 64, 128, 256]
-  }
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
+    pwa: {
+        dest: "public",
+        runtimeCaching
+    },
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en", "no"],
+        defaultLocale: "no",
+        localeDetection: false
+    },
+    images: {
+        formats: ["image/avif", "image/webp"],
+        deviceSizes: [350, 540, 945],
+        imageSizes: [16, 32, 48, 64, 128, 256]
+    }
+});
+
+module.exports = nextConfig;
