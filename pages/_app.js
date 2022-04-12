1557ffdc8f14ff1dc5ed7ee1f0970bc333ed4f45
Benjamin Dehli <superelg@gmail.com>
2022-04-12 22:50:04 +0200

    Fix style rules for FontAwesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,57 @@
 import { useAmp } from "next/amp";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import Layout from "components/Layout";
 import AmpLayout from "components/AmpLayout";
 import "../styles/globals.scss";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFacebookF, faInstagram, faTumblr, faTwitter, faVimeoV, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faBullhorn, faChevronDown, faChevronLeft, faChevronRight, faFilm, faFilter, faGripHorizontal, faGuitar, faListUl, faLanguage, faMusic, faPhotoVideo, faRss, faSearch, faShoppingCart, faSlidersH } from "@fortawesome/free-solid-svg-icons";
+import {
+    faBullhorn,
+    faChevronDown,
+    faChevronLeft,
+    faChevronRight,
+    faFilm,
+    faFilter,
+    faGripHorizontal,
+    faGuitar,
+    faListUl,
+    faLanguage,
+    faMusic,
+    faPhotoVideo,
+    faRss,
+    faSearch,
+    faShoppingCart,
+    faSlidersH
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBullhorn, faChevronDown, faChevronLeft, faChevronRight, faFacebookF, faFilm, faFilter, faGripHorizontal, faGuitar, faInstagram, faLanguage, faListUl, faMusic, faPhotoVideo, faRss, faSearch, faShoppingCart, faSlidersH, faTumblr, faTwitter, faVimeoV, faYoutube);
+library.add(
+    faBullhorn,
+    faChevronDown,
+    faChevronLeft,
+    faChevronRight,
+    faFacebookF,
+    faFilm,
+    faFilter,
+    faGripHorizontal,
+    faGuitar,
+    faInstagram,
+    faLanguage,
+    faListUl,
+    faMusic,
+    faPhotoVideo,
+    faRss,
+    faSearch,
+    faShoppingCart,
+    faSlidersH,
+    faTumblr,
+    faTwitter,
+    faVimeoV,
+    faYoutube
+);
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
     const isAmp = useAmp();
