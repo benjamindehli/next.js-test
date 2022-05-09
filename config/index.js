e6cac5de4febc594f0d140890d873dfe00024cc3
Benjamin Dehli <superelg@gmail.com>
2022-05-09 23:50:55 +0200

    Add config for ImageKit URL variable

--- config/index.js
+++ config/index.js
@@ -4,9 +4,11 @@ const dbClusterUrl = process.env.DB_CLUSTER_URL;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
+const imageKitId = process.env.NEXT_PUBLIC_IMAGEKIT_ID;
 
 export const isProduction = environment !== "productions";
 
 export const server = !isProduction ? "http://localhost:3000" : productionUrl;
 
 export const dbConnectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbClusterUrl}/${dbName}?retryWrites=true&w=majority`;
+export const imageKitUrlEndpoint = `https://ik.imagekit.io/${imageKitId}`;
