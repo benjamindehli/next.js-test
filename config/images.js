8014f62b5a7143d30da3470e4209a509e0aef8ae
Benjamin Dehli <superelg@gmail.com>
2022-05-09 23:50:30 +0200

    Add config for image sizes and formats

---
+++ config/images.js
@@ -0,0 +1,99 @@
+const imageConfigs = {
+    header: {
+        formats: [
+            {
+                formatKey: "avif",
+                mimeType: "image/avif"
+            },
+            {
+                formatKey: "jpg",
+                mimeType: "image/jpg"
+            },
+            {
+                formatKey: "webp",
+                mimeType: "image/webp"
+            }
+        ],
+        sizes: [
+            {
+                width: 480,
+                height: 306
+            },
+            {
+                width: 640,
+                height: 408
+            },
+            {
+                width: 800,
+                height: 510
+            },
+            {
+                width: 1024,
+                height: 653
+            },
+            {
+                width: 1260,
+                height: 804
+            },
+            {
+                width: 1440,
+                height: 919
+            },
+            {
+                width: 1680,
+                height: 1072
+            }
+        ],
+        defaultFormat: {
+            formatKey: "jpg",
+            mimeType: "image/jpg"
+        },
+        defaultSize: {
+            width: 1024,
+            height: 653
+        }
+    },
+    post: {
+        formats: [
+            {
+                formatKey: "avif",
+                mimeType: "image/avif"
+            },
+            {
+                formatKey: "jpg",
+                mimeType: "image/jpg"
+            },
+            {
+                formatKey: "webp",
+                mimeType: "image/webp"
+            }
+        ],
+        sizes: [
+            {
+                width: 55,
+                height: 55
+            },
+            {
+                width: 350,
+                height: 260,
+                maintainRatio: true
+            },
+            {
+                width: 540,
+                height: 400,
+                maintainRatio: true
+            }
+        ],
+        defaultFormat: {
+            formatKey: "jpg",
+            mimeType: "image/jpg"
+        },
+        defaultSize: {
+            width: 350,
+            height: 260,
+            maintainRatio: true
+        }
+    }
+};
+
+export default imageConfigs;
