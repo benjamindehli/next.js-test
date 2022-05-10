3d2410349bdcaa9deed10e40ba1bb3d6c59b469b
Benjamin Dehli <superelg@gmail.com>
2022-05-10 23:40:59 +0200

    Add domain for ImageKit

--- next.config.js
+++ next.config.js
@@ -18,7 +18,8 @@ const nextConfig = withPWA({
     images: {
         formats: ["image/avif", "image/webp"],
         deviceSizes: [350, 540, 945],
-        imageSizes: [16, 32, 48, 64, 128, 256]
+        imageSizes: [16, 32, 48, 64, 128, 256],
+        domains: ['ik.imagekit.io']
     }
 });
 
