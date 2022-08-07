3f851366563362f618a645445ec91d1dd997058f
Benjamin Dehli <superelg@gmail.com>
2022-08-07 17:06:35 +0200

    Add head elements for equipment types page

--- pages/equipment/index.js
+++ pages/equipment/index.js
@@ -12,18 +12,48 @@ import EquipmentTypeList from "components/partials/EquipmentTypeList";
 
 // Helpers
 import { getAllInCollection } from "helpers/databaseHelpers";
+import { getLocaleSlug } from "helpers/urlHelpers";
 
 // JSON-LD
-//import getPostsJsonLd from "json-ld/posts";
-
+import getEquipmentTypesJsonLd from "json-ld/equipmentTypes";
 
 const EquipmentTypes = (props) => {
     const router = useRouter();
     const { locale } = router;
 
+    const metadata = {
+        title: {
+            en: "Equipment | Dehli Musikk",
+            no: "Utstyr | Dehli Musikk"
+        },
+        heading: {
+            en: "Equipment",
+            no: "Utstyr"
+        },
+        description: {
+            en: "Equipment I use during recording",
+            no: "Utstyr jeg bruker under innspilling"
+        }
+    };
+
     return (
         <Fragment>
-           {/* <Head>{getPostsJsonLd(props.posts, locale)}</Head> */}
+            <Head>
+                <title>{metadata.title[locale]}</title>
+                <meta name="description" content={metadata.description[locale]} />
+                <link rel="canonical" href={`https://www.dehlimusikk.no/${props.localeSlug}equipment/`} />
+                <link rel="alternate" href={`https://www.dehlimusikk.no/equipment/`} hreflang="no" />
+                <link rel="alternate" href={`https://www.dehlimusikk.no/en/equipment/`} hreflang="en" />
+                <link rel="alternate" href={`https://www.dehlimusikk.no/equipment/`} hreflang="x-default" />
+                <meta property="og:title" content={metadata.heading[locale]} />
+                <meta property="og:url" content={`https://www.dehlimusikk.no/${props.localeSlug}equipment/`} />
+                <meta property="og:description" content={metadata.description[locale]} />
+                <meta property="og:locale" content={locale === "en" ? "en_US" : "no_NO"} />
+                <meta property="og:locale:alternate" content={locale === "en" ? "nb_NO" : "en_US"} />
+                <meta property="twitter:title" content={metadata.heading[locale]} />
+                <meta property="twitter:description" content={metadata.description[locale]} />
+                {getEquipmentTypesJsonLd(props.equipmentTypes, locale)}
+            </Head>
             <EquipmentTypeList equipmentTypes={props.equipmentTypes} />
         </Fragment>
     );
@@ -31,7 +61,7 @@ const EquipmentTypes = (props) => {
 
 export default EquipmentTypes;
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (context) => {
     const client = await MongoClient.connect(dbConnectionString);
     const equipmentTypes = await getAllInCollection(client, "equipmentTypes").then((equipmentTypes) => {
         return equipmentTypes.map((equipmentType) => {
@@ -42,9 +72,13 @@ export const getStaticProps = async () => {
         });
     });
     client.close();
+
+    const localeSlug = getLocaleSlug(context.locale, context.defaultLocale);
+
     return {
         props: {
-            equipmentTypes
+            equipmentTypes,
+            localeSlug
         }
     };
 };
